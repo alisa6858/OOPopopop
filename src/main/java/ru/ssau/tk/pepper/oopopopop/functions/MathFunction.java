675cc8c6c72d40366a6e126e611d6e79abea2609
alisk <aliskafil@yandex.ru>
2025-09-27 15:21:40 +0300

    MathFunction (доработка)

--- src/main/java/ru/ssau/tk/pepper/oopopopop/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/functions/MathFunction.java
@@ -2,4 +2,7 @@ package ru.ssau.tk.pepper.oopopopop.functions;
 
 public interface MathFunction {
     double apply(double x);
+    default CompositeFunction andThen(MathFunction afterFunction){
+        return new CompositeFunction(this, afterFunction);
+    }
 }
