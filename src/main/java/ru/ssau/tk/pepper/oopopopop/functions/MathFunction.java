71422a00f0982824a3a866a11e7be27067b67010
hopelesscode0512-dotcom <awesome.5palokpinga>
2025-09-27 19:54:08 +0400

    MathFunction

--- src/main/java/ru/ssau/tk/pepper/oopopopop/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/functions/MathFunction.java
@@ -2,4 +2,8 @@ package ru.ssau.tk.pepper.oopopopop.functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
 }
