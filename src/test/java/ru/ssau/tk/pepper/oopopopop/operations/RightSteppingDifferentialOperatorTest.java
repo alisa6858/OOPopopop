8fe1c1fc1f34d6c9d62daef81a4affc30fa08fe7
alisk <aliskafil@yandex.ru>
2025-10-13 13:49:01 +0300

    добавление тестов

---
+++ src/test/java/ru/ssau/tk/pepper/oopopopop/operations/RightSteppingDifferentialOperatorTest.java
@@ -0,0 +1,31 @@
+package ru.ssau.tk.pepper.oopopopop.operations;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk.pepper.oopopopop.functions.MathFunction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RightSteppingDifferentialOperatorTest {
+    private final MathFunction F = Math::log;
+    private final MathFunction DF = x -> 1.0 / x;
+
+    private final double STEP = 0.01;
+
+    @Test
+    void derive() {
+        RightSteppingDifferentialOperator op = new RightSteppingDifferentialOperator(STEP);
+        MathFunction df = op.derive(F);
+
+        double x = 1;
+        double dy = df.apply(x);
+        assertEquals(DF.apply(x), dy, STEP);
+
+        x = 2;
+        dy = df.apply(x);
+        assertEquals(DF.apply(x), dy, STEP);
+
+        x = 3;
+        dy = df.apply(x);
+        assertEquals(DF.apply(x), dy, STEP);
+    }
+}
