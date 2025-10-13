deb2915fb071504ba4a65f4bb65a06e9ccb58635
alisk <aliskafil@yandex.ru>
2025-10-13 11:11:16 +0300

    создание класса InconsistentFunctionsException и обновление TabulatedFunctionOperationService

---
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.pepper.oopopopop.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() {
+        super();
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/ssau/tk/pepper/oopopopop/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,30 @@
 package ru.ssau.tk.pepper.oopopopop.operations;
 
+import ru.ssau.tk.pepper.oopopopop.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.pepper.oopopopop.functions.Point;
 import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
+import ru.ssau.tk.pepper.oopopopop.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.pepper.oopopopop.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] pts = new Point[tabulatedFunction.getCount()];
         int i = 0;
@@ -11,5 +32,39 @@ public class TabulatedFunctionOperationService {
             pts[i++] = pt;
         }
         return pts;
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, Double::sum);
+    }
+
+    public TabulatedFunction sub(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException();
+        }
+
+        Point[] pA = asPoints(a);
+        Point[] pB = asPoints(b);
+
+        double[] xValues = new double[pA.length];
+        double[] yValues = new double[pA.length];
+
+        for (int i = 0; i < pA.length; ++i) {
+            if (pA[i].x != pB[i].x) {
+                throw new InconsistentFunctionsException();
+            }
+            xValues[i] = pA[i].x;
+            yValues[i] = operation.apply(pA[i].y, pB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
     }
 }
