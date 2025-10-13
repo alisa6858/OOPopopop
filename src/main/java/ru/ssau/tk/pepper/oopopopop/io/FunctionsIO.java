4fb2f85e401fe8a40148f47679f09b5bebdc1972
alisk <aliskafil@yandex.ru>
2025-10-13 13:26:03 +0300

    создание TabulatedFunctionFileReader и добавление метода в FunctionsIO и файла в input

--- src/main/java/ru/ssau/tk/pepper/oopopopop/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/io/FunctionsIO.java
@@ -2,27 +2,17 @@ package ru.ssau.tk.pepper.oopopopop.io;
 
 import ru.ssau.tk.pepper.oopopopop.functions.Point;
 import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
-
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
-
-import ru.ssau.tk.pepper.oopopopop.functions.Point;
-import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
+import ru.ssau.tk.pepper.oopopopop.functions.factory.TabulatedFunctionFactory;
 
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
 
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
-    }
-
-    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
-        PrintWriter printWriter = new PrintWriter(writer);
-        printWriter.printf("%d\n", function.getCount());
-        for (Point p: function) {
-            printWriter.printf("%f %f\n", p.x, p.y);
-        }
-        printWriter.flush();
     }
 
     public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
@@ -42,5 +32,25 @@ public final class FunctionsIO {
             stream.writeDouble(p.y);
         }
         stream.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        try {
+            int count = Integer.parseInt(reader.readLine());
+            double[] xValues = new double[count];
+            double[] yValues = new double[count];
+            NumberFormat format = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+            for (int i = 0; i < count; ++i) {
+                String[] xy = reader.readLine().split("\\s");
+                if (xy.length != 2) {
+                    throw new IOException("Invalid file format.");
+                }
+                xValues[i] = format.parse(xy[0]).doubleValue();
+                yValues[i] = format.parse(xy[1]).doubleValue();
+            }
+            return factory.create(xValues, yValues);
+        } catch (NumberFormatException | ParseException e) {
+            throw new IOException(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/io/TabulatedFunctionFileReader.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.pepper.oopopopop.io;
+
+import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
+import ru.ssau.tk.pepper.oopopopop.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.pepper.oopopopop.functions.factory.LinkedListTabulatedFunctionFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TabulatedFunctionFileReader {
+    public static void main(String[] args) {
+
+        try (FileReader in1 = new FileReader("input/function.txt");
+             FileReader in2 = new FileReader ("input/function.txt")) {
+            ArrayTabulatedFunctionFactory ff1 = new ArrayTabulatedFunctionFactory();
+            LinkedListTabulatedFunctionFactory ff2 = new LinkedListTabulatedFunctionFactory();
+
+            TabulatedFunction f1 = FunctionsIO.readTabulatedFunction(new BufferedReader(in1), ff1);
+            System.out.println(f1);
+
+            TabulatedFunction f2 = FunctionsIO.readTabulatedFunction(new BufferedReader(in2), ff2);
+            System.out.println(f2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
