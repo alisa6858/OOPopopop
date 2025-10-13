47bb0796e1e70f3466859a3e9b23d27d49a102b9
alisk <aliskafil@yandex.ru>
2025-10-13 22:53:55 +0300

    добавление метода TabulatedFunctionFactory

--- src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/TabulatedFunctionFactory.java
@@ -4,4 +4,8 @@ import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    TabulatedFunction createStrict(double[] xValues, double[] yValues);
+
+    TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues);
 }
