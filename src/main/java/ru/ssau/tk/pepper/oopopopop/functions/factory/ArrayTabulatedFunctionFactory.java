ff18b8b8c699895dec0a6969d1f87e50206f481e
hopelesscode0512-dotcom <awesome.5palokpinga>
2025-10-13 23:47:43 +0400

    Добавлен абстрактной фабрике TabulatedFunctionFactory метод: TabulatedFunction createStrict(double[] xValues, double[] yValues)

--- src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/ArrayTabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/ArrayTabulatedFunctionFactory.java
@@ -1,11 +1,17 @@
 package ru.ssau.tk.pepper.oopopopop.functions.factory;
 
 import ru.ssau.tk.pepper.oopopopop.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.pepper.oopopopop.functions.StrictTabulatedFunction;
 import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
 
 public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory {
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new ArrayTabulatedFunction(xValues, yValues);
+    }
+
+    @Override
+    public TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(create(xValues, yValues));
     }
 }
--- src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/pepper/oopopopop/functions/factory/TabulatedFunctionFactory.java
@@ -4,4 +4,6 @@ import ru.ssau.tk.pepper.oopopopop.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    TabulatedFunction createStrict(double[] xValues, double[] yValues);
 }
